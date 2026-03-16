486969091cfbb59bd2a333874deb2570ce49fe4e
Floverflow <flo.cinbolat@gmail.com>
2026-03-16 17:42:22 +0100

    perf: parallelize high-volume tests to meet performance requirements

--- TourGuideTest/PerformanceTest.cs
+++ TourGuideTest/PerformanceTest.cs
@@ -1,14 +1,6 @@
 ﻿using GpsUtil.Location;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TourGuide.LibrairiesWrappers.Interfaces;
-using TourGuide.Services.Interfaces;
 using TourGuide.Users;
-using TourGuide.Utilities;
 using Xunit.Abstractions;
 
 namespace TourGuideTest
@@ -17,22 +9,22 @@ namespace TourGuideTest
     {
         /*
          * Note on performance improvements:
-         * 
+         *
          * The number of generated users for high-volume tests can be easily adjusted using this method:
-         * 
+         *
          *_fixture.Initialize(100000); (for example)
-         * 
-         * 
+         *
+         *
          * These tests can be modified to fit new solutions, as long as the performance metrics at the end of the tests remain consistent.
-         * 
+         *
          * These are the performance metrics we aim to achieve:
-         * 
+         *
          * highVolumeTrackLocation: 100,000 users within 15 minutes:
          * Assert.True(TimeSpan.FromMinutes(15).TotalSeconds >= stopWatch.Elapsed.TotalSeconds);
          *
          * highVolumeGetRewards: 100,000 users within 20 minutes:
          * Assert.True(TimeSpan.FromMinutes(20).TotalSeconds >= stopWatch.Elapsed.TotalSeconds);
-        */
+         */
 
         private readonly DependencyFixture _fixture;
 
@@ -44,7 +36,7 @@ namespace TourGuideTest
             _output = output;
         }
 
-        [Fact(Skip = ("Delete Skip when you want to pass the test"))]
+        [Fact]
         public void HighVolumeTrackLocation()
         {
             //On peut ici augmenter le nombre d'utilisateurs pour tester les performances
@@ -55,10 +47,9 @@ namespace TourGuideTest
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            foreach (var user in allUsers)
-            {
-                _fixture.TourGuideService.TrackUserLocation(user);
-            }
+            Parallel.ForEach(allUsers, new ParallelOptions { MaxDegreeOfParallelism = 1000 },
+                user => _fixture.TourGuideService.TrackUserLocation(user));
+
             stopWatch.Stop();
             _fixture.TourGuideService.Tracker.StopTracking();
 
@@ -67,7 +58,7 @@ namespace TourGuideTest
             Assert.True(TimeSpan.FromMinutes(15).TotalSeconds >= stopWatch.Elapsed.TotalSeconds);
         }
 
-        [Fact(Skip = ("Delete Skip when you want to pass the test"))]
+        [Fact]
         public void HighVolumeGetRewards()
         {
             //On peut ici augmenter le nombre d'utilisateurs pour tester les performances
@@ -78,10 +69,13 @@ namespace TourGuideTest
 
             Attraction attraction = _fixture.GpsUtil.GetAttractions()[0];
             List<User> allUsers = _fixture.TourGuideService.GetAllUsers();
-            allUsers.ForEach(u => u.AddToVisitedLocations(new VisitedLocation(u.UserId, attraction, DateTime.Now)));
 
-            allUsers.ForEach(u => _fixture.RewardsService.CalculateRewards(u));
-
+            Parallel.ForEach(allUsers,
+                user => user.AddToVisitedLocations(new VisitedLocation(user.UserId, attraction, DateTime.Now)));
+            
+            Parallel.ForEach(allUsers, new ParallelOptions { MaxDegreeOfParallelism = 1000 },
+                user => _fixture.RewardsService.CalculateRewards(user));
+            
             foreach (var user in allUsers)
             {
                 Assert.True(user.UserRewards.Count > 0);
